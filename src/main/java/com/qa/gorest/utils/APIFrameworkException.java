3ebfc4fd365017d1516c957be607743df3b82c42
Kumar Abhimanyu <kumarabhimanyu941@gmail.com>
2024-08-07 16:26:59 +0530

    added Custom API Framework Exception class

---
+++ src/main/java/com/qa/gorest/utils/APIFrameworkException.java
@@ -0,0 +1,9 @@
+package com.qa.gorest.utils;
+
+public class APIFrameworkException extends RuntimeException {
+
+	public APIFrameworkException(String exceptionMsg) {
+		super(exceptionMsg);
+	}
+
+}
